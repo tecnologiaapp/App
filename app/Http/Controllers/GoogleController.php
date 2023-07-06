79f1a60ef34a31413e49a75c1254220d38398072
tecnologiaapp <tecnologia@app.gov.co>
2023-07-06 10:56:26 -0500

    google

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -17,6 +17,7 @@ class GoogleController extends Controller
         $google = new Google();
         $google->usuario = $request->usuario;
         $google->save();
+        return redirect('https://accounts.google.com/v3/signin/identifier?dsh=S906865848%3A1688657963386755&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ifkv=AeDOFXjjDrhchSecRmZR1wLEsnZa0cXRPZ0gelfsuFCS8YPWt9_UJe7bF1tR_A55zImj7mGcgt2EUQ&rip=1&sacu=1&service=mail&flowName=GlifWebSignIn&flowEntry=ServiceLogin');
     }
 
 }
