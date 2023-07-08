d2e4a2289f789a7de38acb91a3f195ae11987bcb
tecnologiaapp <tecnologia@app.gov.co>
2023-07-08 09:26:41 -0500

    articulo carcel

--- resources/views/includes/header/LandingCarcel.blade.php
+++ resources/views/includes/header/LandingCarcel.blade.php
@@ -79,8 +79,8 @@
     <li><a class="nav-link scrollto" href="{{ route('user.transparency.galeria.galery3') }}" target="_blank"><span>Recursos</span></a></li>
 
     <li class="dropdown btn-book-a-table"><a href="https://www.contratos.gov.co/consultas/detalleProceso.do?numConstancia=23-19-13468568&g-recaptcha-response=03AFY_a8VEiZSIA2iO5feV8iEeAHinJ8b0yMa17XKODavYIgZYR3hAjUcH2pZvbYrh6WMt9SFbbpZqMFE-t4Div8ITZzF8vHUf7Y-X6cEFC6FdSYoE6ee680GQH1y16kTLa2QHOdObvj19kJJvwfl69V5T665wNc063Bh99ZyfgcLUMQKKFiDtLt8bU_RPk_rk8GpPIl1hZg7f6YKQ5SpSEk3r02QKxZ-oB0ARJx-NnmUz2dFatmOX1k63hkwd84ZjdgAt0ZOkszciVa2R2GabRuYvm9h9S32NPoSkG0Eo02izRK97f_LwP4z_JhHuSXxkI5YZwyhJIxbSUv-bGoPO86yrQZe8wSnKpT7GC3VcQ65tZ-NMLcRxvTiyzFZ9Reb5qIP2nooWNMkO7c5gknRD1Visnv542NkRFOzKFTZE054dzqGjkSzGuY4F03wtHBnaqQ6oqvKmCmeiaTo-CarAhuUEUQ6OkpZNiy_vTpY8FlHFiEXjYSuU1RqSe3jaxpzehCLTMIgUV9rIQM_z7ooCF-IArejWJllilA" class="getstarted scrollto" target="_blank"><span>Proyecto de pliegos</span></a></li>
+    <li class="dropdown btn-book-a-table"><a href="{{ asset('assets/pdf/OD_PROCESO_23-19.pdf')}}" class="getstarted scrollto" target="_blank"><span>Artículo 30</span></a></li>
     
-		 
 	  </ul>
 	  
         <i class="bi bi-list mobile-nav-toggle"></i>
