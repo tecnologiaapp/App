141e6e938e5ddfda8f54ba650778415d9b637182
morenoalvarezandres <morenoalvarezandres@gmail.com>
2023-10-04 10:31:34 -0500

    Actualizacion de carousel se quita mes del patrimonio

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -24,17 +24,13 @@
 				<li class="active" data-slide-to="0" data-target="#carouselExampleIndicators"></li>
 				<li data-slide-to="1" data-target="#carouselExampleIndicators"></li>
 				<li data-slide-to="2" data-target="#carouselExampleIndicators"></li>
-				<li data-slide-to="3" data-target="#carouselExampleIndicators"></li>
 			</ol>
 			<div class="carousel-inner">
 				<div class="carousel-item active">
-					<a href="https://patrimoniomedellin.gov.co/"><img alt="One slide" class="d-block w-100" src="assets/img/Banners/prado.jpg"></a>
+					<a href="{{route('user.projects.mudag')}}"><img alt="One slide" class="d-block w-100" src="assets/img/Banners/bannerHome1.jpg"></a>>
 				</div>
 				<div class="carousel-item">
-					<a href="{{route('user.projects.mudag')}}"><img alt="Second slide" class="d-block w-100" src="assets/img/Banners/bannerHome1.jpg"></a>
-				</div>
-				<div class="carousel-item">
-					<a href="{{route('user.noticias.new50')}}"><img alt="Third slide" class="d-block w-100" src="assets/img/Banners/bannerHome2.jpg"></a>
+					<a href="{{route('user.noticias.new50')}}"><img alt="Second slide" class="d-block w-100" src="assets/img/Banners/bannerHome2.jpg"></a>
 				</div>
 				<div class="carousel-item">
 					<a href="https://www.instagram.com/s/aGlnaGxpZ2h0OjE3OTg4MDk0NTkyMDA3ODIx?story_media_id=3115907623191126740_6167483428&igshid=MzRlODBiNWFlZA=="><img alt="Four slide" class="d-block w-100" src="assets/img/Banners/bannerHome3.jpg"></a>
