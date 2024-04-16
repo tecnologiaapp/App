9924b712a2d1ac199234ee43a365ecc4ce27c1c7
jcgiraldos <jcgiraldos@eafit.edu.co>
2024-04-16 16:33:36 -0500

    Direccion tecnica actulización

--- resources/views/user/about/subdirectors/tecnical.blade.php
+++ resources/views/user/about/subdirectors/tecnical.blade.php
@@ -4,34 +4,38 @@
 <section>
 		<!--Page Title Hero-->
 		<div class="vlt-page-title-hero vlt-page-title-hero--work vlt-page-title-hero--lg jarallax">
-			<div class="vlt-page-title-hero__overlay"></div><img alt="" class="jarallax-img" src="{{asset('assets/img/Banners/bannerDirectoraTecnica.jpg')}}">
+			<div class="vlt-page-title-hero__overlay"></div><img alt="" class="jarallax-img" src="{{asset('assets/img/BannersNuevos/BannerTecnica.jpg')}}">
 			<div class="vlt-page-title-hero__content ocultPosition">
-				<h1 style="color:white;">Direccion  Técnica</h1>
+				<h1 style="color:white;">Dirección  Técnica</h1>
 			</div>
 		</div>
 	</section>
 <section>
 <section>
+<div class="text-center">
+						<div class="section-title">
+							
+						<h2>Edison Montoya Vélez</h2>
 		<div class="vlt-gap-120"></div>
 		<div class="container">
 			<div class="row no-gutters">
-			{{--<!-- ======= Hero Section ======= -->
+			<!-- ======= Hero Section ======= -->
 				<section class="d-flex align-items-center" id="hero">
 					<div class="container">
 						<div class="row">
-							<div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
+							{{-- <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
 								<p class="normal-text">La dirección técnica de la Agencia APP está a cargo de Nathalia Gallego Mejía, Arquitecta de la Universidad Nacional de Colombia Sede Medellín, Magíster en Arquitectura de la Universidad de Houston - Texas, con habilidades para la planeación, dirección, gestión y seguimiento de proyectos, con amplia experiencia administrativa y técnica en los sectores hospitalario, educativo, inmobiliario, urbano y de APP's, en el ámbito público y privado.</p>
 								<p class="normal-text">Nathalia ha participado en importantes proyectos de ciudad como los son el diseño y adecuación de espacios urbanos de la ciudad de Medellín - proyecto UVAS (EPM); el apoyo a la supervisión de proyectos hospitalarios como el Centro de Salud San Lorenzo, Unidad Hospitalaria de San Cristóbal, Unidad Hospitalaria de Pajarito; la coordinación del Equipo de Expansión Educativa - Subsecretaría de Planeación Educativa, quienes iniciaron el análisis normativo y de oferta y demanda de la infraestructura educativa de la ciudad de Medellín; el apoyo a la Dirección General de la Agencia APP en el seguimiento a la gestión de los proyectos desarrollados por la Agencia APP como líder de Planeación, apoyando en la formulación del Plan de Desarrollo "Medellín Futuro 2020-2023” y en el seguimiento a las metas establecidas para la entidad.</p>
 								<p class="normal-text">Como directora técnica de la Agencia APP, Nathalia ha logrado consolidar y direccionar un equipo de profesionales idóneos, con alto sentido de responsabilidad, adaptabilidad y compromiso, con actitud positiva, proactiva, propositiva, con buenas relaciones interpersonales y comprometidos hacia el logro de los objetivos financieros, administrativos, contables y de mejora continua planteados, no solo en aras al cumplimiento de las metas del plan de Desarrollo, si no hacia el cumplimiento de la misión y visión de la Agencia APP.</p>
-							</div>
-							<div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200"><img alt="" class="img-fluid animated" src="{{asset('assets/img/funcionarios/imgDirectoraTecnica.png')}}"></div>
+							</div> --}}
+							<div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200"><img alt="" class="img-fluid animated" src="{{asset('assets/img/funcionarios/EdisonMontoyaVelez.png')}}"></div>
 						</div>
 					</div>
 				</section><!-- End Hero -->
 			</div>
 		</div>
 	</section>
-	<footer class="vlt-single-post__footer">--}}
+	<footer class="vlt-single-post__footer">
 		<!--Post Tags-->
 		<!--Post Share-->
 		<section style="background-color: transparent;">
