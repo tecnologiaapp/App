3e9bf65fb003d05fbe2f7cd14e61399a8050d19a
jcgiraldos <jcgiraldos@eafit.edu.co>
2024-07-08 14:27:50 -0500

    Informes pqrs 2024-1

--- resources/views/user/transparency/control.blade.php
+++ resources/views/user/transparency/control.blade.php
@@ -311,7 +311,19 @@
 				</div>
 			</div><button class="accordion">Informes de peticiones, quejas, reclamos y sugerencias - PQRS</button>
 			<div class="panel">
-				<button class="accordion">2023</button>
+			<button class="accordion">2024</button>
+			<div class="panel">
+					<table class="table table-bordered table-striped custom-table">
+						<tbody>
+							<tr>
+								<td>Informe de PQRSD - Primer trimestre de 2024</td>
+								<td>
+									<a class="btn btn-primary btn-block" href="{{asset('assets/pdf/pqrs/Informe-de-PQRSD-PrimerTrismestre2024.pdf')}}" target="_blank">Ver documento</a>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+			</div><button class="accordion">2023</button>
 				<div class="panel">
 					<table class="table table-bordered table-striped custom-table">
 						<tbody>
