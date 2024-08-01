16421fd351f96b86a35edd90c0612d100e342610
jcgiraldos <jcgiraldos@eafit.edu.co>
2024-08-01 16:14:57 -0500

    MAPA DE RIESGO DE CORRUPCIÓN 2024

--- resources/views/user/transparency/control.blade.php
+++ resources/views/user/transparency/control.blade.php
@@ -76,6 +76,19 @@
 				</div>
 			</div><button class="accordion">Informes de control</button>
 			<div class="panel">
+			<button class="accordion">2024</button>
+				<div class="panel">
+					<table class="table table-bordered table-striped custom-table">
+						<tbody>
+							<tr>
+								<td>Mapa de riesgos de corrupción de 2024</td>
+								<td>
+									<a class="btn btn-primary btn-block" href="{{asset('assets/pdf/control/Mapa-de-Riesgos-de-Corrupción-2024.xlsx')}}" target="_blank">Ver documento</a>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
 				<button class="accordion">2023</button>
 				<div class="panel">
 					<table class="table table-bordered table-striped custom-table">
