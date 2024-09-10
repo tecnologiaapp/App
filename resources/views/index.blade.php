eddf533cf49f32c501d96ce1904153e91962d65c
jcgiraldos <jcgiraldos@eafit.edu.co>
2024-09-10 12:29:54 -0500

    Cambio de orden banners

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,8 +16,8 @@
 				</ol>
 			<div class="carousel-inner">
 			    <div class="carousel-item">
-			       <a href="{{asset('assets/pdf/BASES-DE-CONCURSO-DE-FOTOGRAFÍA.pdf')}}"><img class="d-block w-100" src="{{asset('assets/img/BannersNuevos/Banner-concurso-de-fotografía.png')}}"></a>
-			   </div>
+				    <a href="{{asset('assets/pdf/Carta-de-trato-digno.pdf')}}"><img class="d-block w-100" src="{{asset('assets/img/Banners/CartaTratoDigno.png')}}"></a>
+				</div>
 				<div class="carousel-item">
 					<a href="{{asset('assets/pdf/Arquitectura-Moderna.pdf')}}"><img class="d-block w-100" src="{{asset('assets/img/Banners/Banner-web_Mesadetrabajo.jpg')}}"></a>
 				</div>
@@ -27,10 +27,9 @@
 				<div class="carousel-item">
 					<a href="{{route('user.noticias.new50')}}"><img  class="d-block w-100" src="{{asset('assets/img/BannersNuevos/HomeConstelaciones.jpg')}}"></a>
 				</div>
-			<div class="carousel-item active">
-					<a href="{{asset('assets/pdf/Carta-de-trato-digno.pdf')}}"><img class="d-block w-100" src="{{asset('assets/img/Banners/CartaTratoDigno.png')}}"></a>
-				</div>
-			</div><button class="carousel-control-prev" data-slide="prev" data-target="#carouselExampleIndicators" onclick="disableScroll()" type="button"><span aria-hidden="true" class="carousel-control-prev-icon"></span> <span class="sr-only">Previous</span></button> <button class="carousel-control-next" data-slide="next" data-target="#carouselExampleIndicators" onclick="disableScroll()" type="button"><span aria-hidden="true" class="carousel-control-next-icon"></span> <span class="sr-only">Next</span></button>
+			    <div class="carousel-item active">
+			        <a href="{{asset('assets/pdf/BASES-DE-CONCURSO-DE-FOTOGRAFÍA.pdf')}}"><img class="d-block w-100" src="{{asset('assets/img/BannersNuevos/Banner-concurso-de-fotografía.png')}}"></a>		
+		        </div><button class="carousel-control-prev" data-slide="prev" data-target="#carouselExampleIndicators" onclick="disableScroll()" type="button"><span aria-hidden="true" class="carousel-control-prev-icon"></span> <span class="sr-only">Previous</span></button> <button class="carousel-control-next" data-slide="next" data-target="#carouselExampleIndicators" onclick="disableScroll()" type="button"><span aria-hidden="true" class="carousel-control-next-icon"></span> <span class="sr-only">Next</span></button>
 		</div> 
 	</section> 
 	<section class="about" id="about">
