a525f7e33842994e1e66ef42b47b806fcf9ea434
jcgiraldos <jcgiraldos@eafi.edu.co>
2025-03-27 16:02:09 -0500

    Informe de Derecho de Autor - 2024(2)

--- resources/views/user/transparency/control.blade.php
+++ resources/views/user/transparency/control.blade.php
@@ -895,16 +895,16 @@
 						</tbody>
 					</table>
 				</div>
-			</div><button class="accordion">Informe de Derecho Autor de Software</button>
+			</div><button class="accordion">Informe de Derecho de Autor de Software</button>
 			<div class="panel">
 			<button class="accordion">2024</button>
 				<div class="panel">
 					<table class="table table-bordered table-striped custom-table">
 						<tbody>
 							<tr>
-								<td>Informe de Derecho Autor de Software</td>
+								<td>Informe de Derecho de Autor de Software</td>
 								<td>
-									<a class="btn btn-primary btn-block" href="{{asset('assets/pdf/control/INFORME-DE-SOFTWARE-LEGAL-2023.pdf')}}" target="_blank">Ver documento</a>
+									<a class="btn btn-primary btn-block" href="{{asset('assets/pdf/control/InformedeDerechodeAutordeSoftware-2024.pdf')}}" target="_blank">Ver documento</a>
 								</td>
 							</tr>
 						</tbody>
@@ -915,7 +915,7 @@
 					<table class="table table-bordered table-striped custom-table">
 						<tbody>
 							<tr>
-								<td>Informe de Derecho Autor de Software</td>
+								<td>Informe de Derecho de Autor de Software</td>
 								<td>
 									<a class="btn btn-primary btn-block" href="{{asset('assets/pdf/control/INFORME-DE-SOFTWARE-LEGAL-2023.pdf')}}" target="_blank">Ver documento</a>
 								</td>
