837d03e550554d5a569cae1f71aaa320ebea102e
jcgiraldos <jcgiraldos@eafi.edu.co>
2025-04-09 16:16:03 -0500

    Botón Laboratorio de Patrimonio

--- resources/views/user/projects/minorista.blade.php
+++ resources/views/user/projects/minorista.blade.php
@@ -1,41 +1,61 @@
 @extends('layouts.app')
 @section('content')
 @section('body_color', '#FFFFFF;')
+
+<style>
+
+.buttom-accion {
+	max-width: 800px;
+	width: 90%;
+	cursor: pointer;
+	display: block;
+	margin: auto;
+	background-color: #00a6b9;
+	font-size: 25px;
+	text-align: center;
+	color: #FFF;
+	padding: 20px;
+	margin-bottom: 0;
+	border-radius: 5px;
+}
+
+.buttom-accion h1 {
+	font-family: "jost", sans-serif !important;
+	font-weight: bold;
+	font-size: 25px;
+	text-align: center;
+	color: #FFF;
+	margin-bottom: 0;
+}
+
+.buttom-accion:hover {
+	background-color: #089299;
+}
 	
+</style>
+
 <section>
 		<!--Page Title Hero-->
 		<div class="vlt-page-title-hero vlt-page-title-hero--work vlt-page-title-hero--lg jarallax">
-			<div class="vlt-page-title-hero__overlay"></div><img alt="" class="jarallax-img" src="{{asset('assets/img/bg/4.jpg')}}">
-			<div class="vlt-page-title-hero__content">
-				<div class="container">
-					<div class="row">
-						<div class="col-md-8 offset-md-2">
-							<h1 class="vlt-page-title-hero__title" style="text-transform: none;">Plaza Minorista</h1>
-						</div>
-					</div>
-				</div>
-			</div><a class="vlt-scroll-to-arrow" href="#content"><i class="leedo-download-arrow"></i></a>
+			<div class="vlt-page-title-hero__overlay"></div><img alt="" class="jarallax-img" src="{{asset('assets/img/Banners/bannerPlanIntegralDeGestionDePrado.jpg')}}">
+			<div class="vlt-page-title-hero__content ocultPosition" style="width: 100%">
+				<h1 style="color:white;">Laboratorio Vivo de Patrimonio Medellín</h1>
+			</div>
 		</div>
-		<div id="content"></div>
 	</section><!--Content-->
 	<section>
 		<div class="vlt-gap-0"></div>
 		<div class="container">
-			<div class="row">
-				<div class="col-sm-12 col-md-3 offset-md-9">
-					<div class="vlt-particle d-flex justify-content-center" data-jarallax-element="100 0"><img alt="" src="{{asset('assets/img/root/particle-02.png')}}"></div>
-				</div>
-			</div>
 			<section>
 				<div class="container">
 					<div class="row">
 						<div class="col-md-10 offset-md-1">
 							<div class="text-justify">
-								<div class="vlt-gap-15"></div>
-								<hr>
-								<p>Renovación de la infraestructura actual de la central de abastos de la ciudad que busca garantizar el correcto acopio, la seguridad agroalimentaria y el acceso adecuado y de calidad a toda la población, así como aportar a la revitalización urbana de la zona norte del centro de la ciudad y dinamizar el turismo gastronómico. Con este proyecto se espera beneficiar a más de 12.000 comerciantes, productores y visitantes, mejorar las condiciones de los espacios, ser un atractivo turístico, gastronómico y contribuir a la renovación de las plazas de mercado del país.</p>
-								<hr>
-								<div align="center"><img alt="" src="{{asset('assets/img/Proyectos/Minorista.webp')}}"></div>
+								
+								<p style="margin-top: 40px">El Laboratorio Vivo es una metodología de innovación abierta centrada en el usuario para hacer investigación más cercana a la realidad. Se basa en la co-creación, la experimentación y la evaluación de soluciones innovadoras en entornos del mundo real. En la Agencia APP tenemos el objetivo de conformar un ecosistema creativo e inclusivo que impulse y revitalice los valores patrimoniales de Medellín, a través de la innovación abierta, la participación activa de las comunidades, y la gestión eficiente y sostenible de los recursos culturales de la ciudad. ​El laboratorio fomentará la co-creación entre actores locales, instituciones, y sectores creativos para generar soluciones innovadoras que promuevan la preservación, reinterpretación y aprovechamiento del patrimonio cultural como motor de desarrollo social, cultural y económico.​
+								</p>
+								
+								<div align="center"><img alt="" src="{{asset('assets/img/Proyectos/IluminaciónHotelNutibara.jpeg')}}"></div>
 							</div>
 						</div>
 					</div>
@@ -43,21 +63,25 @@
 			</section>
 		</div>
 	</section>
+
+	<br><br>
+	<a href="{{asset('assets/pdf/PresentaciónLivingPatLabMedellín.pdf')}}" target="_blank">
+	<div class="buttom-accion">Haz clic aquí para conocer los detalles del<br><h1>LABORATORIO VIVO DE PATRIMONIO MEDELLÍN</h1></div>
+	</a>
+
 	<footer class="vlt-single-post__footer">
 		<!--Post Tags-->
 		<!--Post Share-->
 		<section style="background-color: transparent;">
-			<div class="vlt-gap-100"></div>
+			
 			<div class="container">
 				<!--Animation Block-->
 				<div class="vlt-animation-block aos-init aos-animate" data-aos="fade" data-aos-delay="0">
 					<!--Social Share-->
-					<div class="vlt-social-share text-center">
-						<h5 class="vlt-display-1">Compártelo</h5><a class="vlt-social-icon vlt-social-icon--style-1 twitter" href="https://twitter.com/intent/tweet?text=https://www.app.gov.co/proyectos/minorista" rel="nofollow" target="_blank"><i class="fa fa-twitter"></i></a> <a class="vlt-social-icon vlt-social-icon--style-1 facebook" href="https://www.facebook.com/sharer/sharer.php?u=https://www.app.gov.co/proyectos/minorista" rel="nofollow" target="_blank"><i class="fa fa-facebook"></i></a>
-					</div>
+					
 				</div>
 			</div>
-			<div class="vlt-gap-100"></div>
+			
 		</section>
 	</footer>
 
